3365bdbf78b2293998fdc9a66403fffa28ed9b33
Nicholas King <enotirab@mail.fresnostate.edu>
2022-01-29 12:53:46 -0800

    initial commit

---
+++ controllers/todoController.js
@@ -0,0 +1,123 @@
+
+const { Todo } = require('../models');
+
+
+module.exports.listAll = async function (req, res, next) {
+
+    try {
+
+        const todos = await Todo.findAll();
+
+        let completeItems = todos.filter(item => item.complete);
+        let incompleteItems = todos.filter(item => !item.complete);
+
+        res.render('all_todos', {
+            completeItems,
+            incompleteItems
+        });
+
+    } catch (err) {
+        console.log('there was an error');
+        res.send(err);
+    }
+
+};
+
+
+module.exports.displayAddItem = (req, res) => {
+    res.render('newItem')
+};
+
+module.exports.addNewItem = async (req, res) => {
+
+    try {
+
+        await Todo.create({description: req.body.description});
+        res.redirect('/');
+    } catch (err) {
+
+        console.log('Error: ' + err);
+    }
+};
+
+
+module.exports.viewEditItem = async (req, res) => {
+
+    try {
+        const todo = await Todo.findByPk(req.params.id);
+        res.render('editItem', {item: todo})
+
+    } catch (err) {
+        console.log('There was an error');
+        console.log(err);
+    }
+
+};
+
+
+module.exports.saveEditItem = async (req, res) => {
+
+    await Todo.update({ description: req.body.description}, {
+        where:{
+            id: req.params.id,
+        }
+    })
+    res.redirect('/');
+};
+
+
+module.exports.deleteItem = async (req, res) => {
+
+    try {
+
+        await Todo.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+    } catch (err) {
+        console.log("there was an error");
+        console.log(err);
+    }
+
+    res.redirect('/');
+};
+
+
+module.exports.makeItemComplete = async (req, res) => {
+
+    try {
+
+        await Todo.update({ complete:  true}, {
+            where:{
+                id: req.params.id,
+            }
+        })
+
+    } catch (err) {
+        console.log("there was an error");
+        console.log(err);
+    }
+
+    res.redirect('/');
+};
+
+
+module.exports.markItemIncomplete = async (req, res) => {
+
+    try {
+        await Todo.update({ complete:  false}, {
+            where:{
+                id: req.params.id,
+            }
+        })
+
+    } catch (err) {
+        console.log("there was an error");
+        console.log(err);
+    }
+
+    res.redirect('/');
+};
+
+
---
+++ models/todo.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Todo.init({
+    description: DataTypes.STRING,
+    complete: DataTypes.BOOLEAN,
+    completeText: {
+      type: DataTypes.VIRTUAL,
+      get(){
+        return this.complete ? 'Complete' : 'Incomplete'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Todo',
+    tableName: 'todo_items',
+    timestamps: false,
+  });
+  return Todo;
+};
