3ab6c609379010fec0653f3bc38a84c4b5eec422
missbolin <samanthabolin1@gmail.com>
2022-02-09 15:17:46 -0800

    save this

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,30 +1,20 @@
-
 const { Todo } = require('../models');
 
 
-module.exports.listAll = async function (req, res, next) {
+module.exports.listAll = async function(req, res, next) {
+    const todos = await Todo.findAll();
 
-    try {
+    let completeItems = todos.filter(item => item.complete);
+    let incompleteItems = todos.filter(item => !item.complete);
 
-        const todos = await Todo.findAll();
-
-        let completeItems = todos.filter(item => item.complete);
-        let incompleteItems = todos.filter(item => !item.complete);
-
-        res.render('todos/viewAll', {
-            completeItems,
-            incompleteItems
-        });
-
-    } catch (err) {
-        console.log('there was an error');
-        res.send(err);
-    }
-
+    res.render('todos/viewAll', {
+        completeItems,
+        incompleteItems
+    });
 };
 
 
-module.exports.displayAddItem = (req, res) => {
+module.exports.displayAddItem = function(req, res) {
     const item = {
         name: '',
         description: '',
@@ -34,35 +24,20 @@ module.exports.displayAddItem = (req, res) => {
     })
 };
 
-module.exports.addNewItem = async (req, res) => {
-
-    try {
-
-        await Todo.create({description: req.body.description});
-        res.redirect('/');
-    } catch (err) {
-
-        console.log('Error: ' + err);
-    }
-};
-
-
-module.exports.viewEditItem = async (req, res) => {
-
-    try {
-        const todo = await Todo.findByPk(req.params.id);
-        res.render('todos/editItem', {item: todo})
-
-    } catch (err) {
-        console.log('There was an error');
-        console.log(err);
-    }
+module.exports.addNewItem = async function(req, res){
+    await Todo.create({description: req.body.description});
+    res.redirect('/');
 
 };
 
 
-module.exports.saveEditItem = async (req, res) => {
+module.exports.viewEditItem = async function(req, res) {
+    const todo = await Todo.findByPk(req.params.id);
+    res.render('todos/editItem', {item: todo})
+};
 
+
+module.exports.saveEditItem = async function(req, res) {
     await Todo.update({ description: req.body.description}, {
         where:{
             id: req.params.id,
@@ -72,57 +47,32 @@ module.exports.saveEditItem = async (req, res) => {
 };
 
 
-module.exports.deleteItem = async (req, res) => {
-
-    try {
-
-        await Todo.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-    } catch (err) {
-        console.log("there was an error");
-        console.log(err);
-    }
-
+module.exports.deleteItem = async function(req, res) {
+    await Todo.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
     res.redirect('/');
 };
 
 
-module.exports.makeItemComplete = async (req, res) => {
-
-    try {
-
-        await Todo.update({ complete:  true}, {
-            where:{
-                id: req.params.id,
-            }
-        })
-
-    } catch (err) {
-        console.log("there was an error");
-        console.log(err);
-    }
-
+module.exports.makeItemComplete = async function(req, res) {
+    await Todo.update({ complete:  true}, {
+        where:{
+            id: req.params.id,
+        }
+    })
     res.redirect('/');
 };
 
 
-module.exports.markItemIncomplete = async (req, res) => {
-
-    try {
-        await Todo.update({ complete:  false}, {
-            where:{
-                id: req.params.id,
-            }
-        })
-
-    } catch (err) {
-        console.log("there was an error");
-        console.log(err);
-    }
-
+module.exports.markItemIncomplete = async function(req, res) {
+    await Todo.update({ complete:  false}, {
+        where:{
+            id: req.params.id,
+        }
+    })
     res.redirect('/');
 };
 
